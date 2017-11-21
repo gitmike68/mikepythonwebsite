871592c44842168ed29b2349064b431b283feaaf
gitmike68 <git_mike_git@mail.ru>
2017-11-21 17:47:34 +0300

    настройки папок сервера

--- script/!all.sh
+++ script/!all.sh
@@ -2,5 +2,6 @@
 sudo rm -r /home/box
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo git clone https://github.com/gitmike68/mikepythonwebsite /home/box/
+#
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
