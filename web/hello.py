fd6e9b119e88706dd32a8707e4cc88ee73917b84
gitmike68 <git_mike_git@mail.ru>
2017-11-21 23:46:57 +0300

    написано Python приложение для обработки запросов Application сервера

--- web/hello.py
+++ web/hello.py
@@ -2,9 +2,12 @@ def application(environ, start_response):
     status="200 OK"
     headers=[("Content-Type","text/plain")]
     body=""
+    '''
     if "QUERY_STRING" in environ:
         for param in environ["QUERY_STRING"].split("&"):
             body+=param
+    '''
+    body="Hello world"
     start_response(status,headers)
 
     return [body]
