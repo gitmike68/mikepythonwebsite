e202597cbac05ceaaf541db01b5a3d61da88bdc1
gitmike68 <git_mike_git@mail.ru>
2017-11-21 23:43:09 +0300

    написано Python приложение для обработки запросов Application сервера

--- script/lesson2.sh
+++ script/lesson2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo rm /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo cd /home/box/web/
+cd /home/box/web/
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello
