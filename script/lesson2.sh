530b1c0ccbc96bc23dcb18c976213cb68bea8c4c
gitmike68 <git_mike_git@mail.ru>
2017-11-21 23:01:36 +0300

    написано Python приложение для обработки запросов Application сервера

--- script/lesson2.sh
+++ script/lesson2.sh
@@ -2,3 +2,4 @@
 sudo rm /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
+sudo gunicorn -c /etc/gunicorn.d/hello.py /home/box/web/hello.py
--- web/etc/hello.py
+++ web/etc/hello.py
@@ -1,10 +1,1 @@
-def lesson2(environ, start_response):
-    status="200 OK"
-    headers=[("Content-Type","text/plain")]
-    body=""
-    if "QUERY_STRING" in environ:
-        for param in environ["QUERY_STRING"].split("&"):
-            body+=param
-    start_response(status,headers)
-
-    return [body]
+bind = "0.0.0.0:8080"
