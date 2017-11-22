3ded28c60bf90e22d7e293e9c0d55adc07ca1e37
gitmike68 <git_mike_git@mail.ru>
2017-11-22 14:00:42 +0300

    настроен скрипт запуска Django из Gunicorn

--- script/!all.sh
+++ script/!all.sh
@@ -8,4 +8,4 @@ sudo git clone -b lesson3 https://github.com/gitmike68/mikepythonwebsite /home/b
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo bash /home/box/script/lesson3.sh
+#sudo bash /home/box/script/lesson3.sh
