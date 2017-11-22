78fbd3e8f6b420c4b590e77089e61f0e62277d79
gitmike68 <git_mike_git@mail.ru>
2017-11-22 12:29:26 +0300

    меняем скрипт для перехода на версию lesson3

--- script/!all.sh
+++ script/!all.sh
@@ -2,7 +2,8 @@
 sudo rm -r /home/box
 sudo rm /etc/nginx/sites-enabled/test.conf
 #sudo git clone https://github.com/gitmike68/mikepythonwebsite /home/box/
-sudo git clone -b lesson2 https://github.com/gitmike68/mikepythonwebsite /home/box/
+#sudo git clone -b lesson2 https://github.com/gitmike68/mikepythonwebsite /home/box/
+sudo git clone -b lesson3 https://github.com/gitmike68/mikepythonwebsite /home/box/
 #
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
--- web/hello.py
+++ web/hello.py
@@ -1,10 +1,9 @@
 def application(environ, start_response):
-    status="200 OK"
-    headers=[("Content-Type","text/plain")]
-    body=""
+    status = "200 OK"
+    headers = [("Content-Type", "text/plain")]
+    body = ""
     if "QUERY_STRING" in environ:
         for param in environ["QUERY_STRING"].split("&"):
-            body+=param+"\n"
-    start_response(status,headers)
-
+            body += param + "\n"
+    start_response(status, headers)
     return [body.encode()]
