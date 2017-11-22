55729cfb122f51c5ad762c286b56acfdd6e3c51a
gitmike68 <git_mike_git@mail.ru>
2017-11-22 19:50:50 +0300

    переходим на ветку ветку lesson4

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render, render_to_response
 
 
 def test(request, *args, **kwargs):
-    num = len(args)
+    if len(args) > 0:
+        num = args[0]
+    else:
+        num = "OK"
     return HttpResponse(str(num))
