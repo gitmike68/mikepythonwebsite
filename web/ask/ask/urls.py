ffcdae488e59267348c9ead27641be634358ce14
gitmike68 <git_mike_git@mail.ru>
2017-11-22 15:30:04 +0300

    nginx настроен на роутинг всех URL задания на Gunicorn

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -21,7 +21,7 @@ from qa.views import test
 urlpatterns = [
     url(r'$', test),
     url(r'signup/$', test),
-    url(r'/question/(\d+)/$', test),
+    url(r'/question/(\d+)/', test),
     url(r'ask/$', test),
     url(r'popular/$', test),
     url(r'new/$', test)
