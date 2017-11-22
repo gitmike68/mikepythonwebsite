bc5b3248579608dae1b24cfbb9179150a0f0ef89
gitmike68 <git_mike_git@mail.ru>
2017-11-22 14:12:40 +0300

    изменены настройки nginx -> gunicorn

--- script/lesson3.sh
+++ script/lesson3.sh
@@ -3,7 +3,7 @@ sudo rm /etc/gunicorn.d/hello.py
 sudo rm -r /etc/gunicorn.d
 sudo mkdir /etc/gunicorn.d
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-cd /home/box/web/
+#cd /home/box/web/
 #sudo gunicorn -c /etc/gunicorn.d/hello.py hello
 cd /home/box/web/ask/
 sudo gunicorn ask.wsgi -c /home/box/web/etc/ask.py
