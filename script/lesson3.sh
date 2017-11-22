14dd9e30dbbcf0d227c8b8b7810a431737c789b7
gitmike68 <git_mike_git@mail.ru>
2017-11-22 13:57:45 +0300

    настроен скрипт запуска Django из Gunicorn

--- script/lesson3.sh
+++ script/lesson3.sh
@@ -4,4 +4,6 @@ sudo rm -r /etc/gunicorn.d
 sudo mkdir /etc/gunicorn.d
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 cd /home/box/web/
-sudo gunicorn -c /etc/gunicorn.d/ask.py hello
+#sudo gunicorn -c /etc/gunicorn.d/hello.py hello
+cd /home/box/web/ask/
+sudo gunicorn ask.wsgi -c /home/box/web/etc/ask.py
