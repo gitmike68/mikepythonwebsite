8f79a92aa31958b3d6536470b91d7e8e0d31c4d3
gitmike68 <git_mike_git@mail.ru>
2017-11-24 17:16:23 +0300

    сделаны модели в файле models.py и скорректированы настройки подключения к СУБД MySQL

--- script/all.sh
+++ script/all.sh
@@ -9,4 +9,5 @@ sudo git clone -b lesson5 https://github.com/gitmike68/mikepythonwebsite /home/b
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo bash /home/box/script/permissions.sh
 #sudo bash /home/box/script/lesson5.sh
